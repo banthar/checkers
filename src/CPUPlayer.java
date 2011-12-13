be86f20a85616453bc10c739beb5a110fb69deab
Piotr Praszmo <ppraszmo@gmail.com>
2011-12-13 14:19:39 +0100

    random CPUPlayer

--- src/CPUPlayer.java
+++ src/CPUPlayer.java
@@ -1,8 +1,16 @@
-
+import java.util.List;
+import java.util.Random;
 
 public class CPUPlayer
 {
 
+	Random rand = new Random();
 	
+	Move getMove(Board board)
+	{
+		List<Move> moves=board.getMoves();
+		
+		return moves.get(rand.nextInt(moves.size()));
+	}
 	
 }
